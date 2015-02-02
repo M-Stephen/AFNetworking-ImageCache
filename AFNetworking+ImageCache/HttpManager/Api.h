198af063eb215e943dfc3583336dc84cff6c4255
Finky <truestyle2005@163.com>
2015-02-02 16:18:51 +0800

    增加了API.h  可以在里面define api

---
+++ AFNetworking+ImageCache/HttpManager/Api.h
@@ -0,0 +1,14 @@
+//
+//  Api.h
+//  AFNetworking+ImageCache
+//
+//  Created by truestyle on 15/2/2.
+//  Copyright (c) 2015年 truestyle. All rights reserved.
+//
+
+#ifndef __API_H__
+#define __API_H__
+
+
+
+#endif
