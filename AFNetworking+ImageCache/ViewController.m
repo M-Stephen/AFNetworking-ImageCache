733c2d6c048d27e9186f4de8b699a95f0eddbbdb
Finky <truestyle2005@163.com>
2015-02-02 10:52:38 +0800

    Initial Commit

---
+++ AFNetworking+ImageCache/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AFNetworking+ImageCache
+//
+//  Created by truestyle on 15/2/2.
+//  Copyright (c) 2015年 truestyle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AFNetworking+ImageCache/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AFNetworking+ImageCache
+//
+//  Created by truestyle on 15/2/2.
+//  Copyright (c) 2015年 truestyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
